9be3d4452c876deff4c856c67383217d78729e68
c0d0er <laeuferk@gmail.com>
2017-03-22 21:59:36 -0700

    Update bscript.js

--- bscript.js
+++ bscript.js
@@ -3,6 +3,7 @@ class Bar extends React.Component {
     $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', (data)=>{
       let info=data.data;
       let info1=data;
+      let prev;//to set up different color of gdp bar;
       const margin = {
         top: 60,
         right: 80,
@@ -12,11 +13,9 @@ class Bar extends React.Component {
           let w=1000-margin.right-margin.left;
     let h=500-margin.top-margin.bottom;
     let dollarSign=d3.format('$,.2f');
-
     const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 console.log(info)
-
     /*let xScale=d3.scaleLinear()
                  .domain([0, d3.max(info, d=>d[1])])
                  //.range([padding, w-padding*2]);
@@ -25,6 +24,7 @@ console.log(info)
     let yScale=d3.scaleLinear()
                  .domain([0, d3.max(info, d=>d[1])])
                  .range([h, 0]);
+let duration = 500, delay = 50;
 
     let minTime=new Date(info[0][0]); 
     let maxTime=new Date(info[info.length-1][0]);
@@ -45,19 +45,31 @@ console.log(info)
            .data(info)
            .enter()
            .append('rect')
-           .attr('x', (d,i)=>i*(w/info.length))
-           .attr('y', (d)=>yScale(d[1]))
+           //.attr('x', (d,i)=>i*(w/info.length))//without chart animation/grows to bottom;
+           .attr('x', 0)//make chart grow to top and from left
+           //.attr('y', (d) => yScale(d[1]))//without chart animation/grows to bottom;
+           .attr('y', h)//make chart grows to top;//make chart grow to top and from left
            .attr('width', w/info.length)
-           .attr('height', (d)=>h-yScale(d[1]))
-           .attr('fill', '#2b5797')
+           //.attr('height', (d)=>h-yScale(d[1]))//without chart animation;
+           .attr('height', 0)//make chart grows to bottom;
+           //.attr('height', 0)//make chart grow to top and from left
+           //.attr('fill', '#2b5797')
+           .attr('fill', d=>{
+            if(prev>d[1]){
+              prev=d[1]; return '#ee1111';
+            } else {
+              prev=d[1]; return '#00a300';
+            }
+           })
            .attr('stroke', '#2b5797')
+           
       .on('mouseover', function(d){
 
             let year=d[0].slice(0,4);
             let month=Number(d[0].slice(5,7))-1;
             d3.select(this)
-              //.transition(250)
-              .attr('fill', '#b2b2b2');
+              //.attr('fill', '#b2b2b2');
+              .style('opacity', '0.3')
 
 div.html('<div class="tooltip1"><span class="gdp">'+dollarSign(d[1])+' Billion</span></span><br><span class="year">'
   +year+'-'+months[month]+'</span></div>')
@@ -69,14 +81,30 @@ div.html('<div class="tooltip1"><span class="gdp">'+dollarSign(d[1])+' Billion</
            })
            .on('mouseout', function(){
             d3.select(this)
-              //.transition(250)
-              .attr('fill', '#2b5797');
+            .style('opacity', '1')
+             // .attr('fill', '#2b5797');
             d3.select('.tooltip1')
               .classed('hidden', true);
-
-           });
-           //.append('svg:title')
-           //.text(d=>d[1]);
+           })
+           .transition()
+           .duration( function () { return duration += 25; })
+        //.ease(d3.easeElasticInOut)
+        .ease((...args) => {
+          let ease = d3.easeElasticInOut(...args);
+          return ease < 0 ? 0 : ease;
+        })
+        //.ease(d3.easeBounceOut)
+       .delay( function () { return delay += 3; })
+      //.duration(10000)
+      //.ease(d3.easeExpOut)
+      //.delay(500)
+      //.attr("x", (d,i)=>i*(w/info.length))//make chart grows to bottom;
+      .attr("x", (d,i)=>i*(w/info.length))//make chart grows to top and bottom;
+     // .attr("y", (d)=>yScale(d[1]))//make chart grows to top;
+     .attr("y", (d)=>yScale(d[1]))//make chart grow to top and from left
+      .attr("height", (d)=>h-yScale(d[1]));//make chart grows to top and bottom;
+           //.append('svg:title')//simple tooltip
+           //.text(d=>d[1]);//simple tooltip
 
         svg
         .append('g')
@@ -106,16 +134,11 @@ div.html('<div class="tooltip1"><span class="gdp">'+dollarSign(d[1])+' Billion</
         .style('font-size', '0.8em')
         .style("text-anchor", "middle")
         .text(info1.description.slice(info1.description.length/2+61))
-
          //.on('click', ()=>{
          // window.open(info1.description.slice(info1.description.length/2+63, info1.description.length-1))
        // });
-
         let concatFormat = (format, suffix) => 
-            (d) => d3.format(format)(d) + suffix;
-
- 
-        
+            (d) => d3.format(format)(d) + suffix;        
         svg.append('g')
         .attr('class', 'axis')
            .attr('transform', 'translate('+0+',0)')
@@ -138,12 +161,8 @@ div.html('<div class="tooltip1"><span class="gdp">'+dollarSign(d[1])+' Billion</
 
   render (){
     return (
-      <div className='main'>
       <div className='chart'>
-      </div>
-      <div className='notes'>
-      </div>
-      </div>
+      </div>     
     )
   }
 }
